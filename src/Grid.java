648297d475bc962ffd9e341a517e111fe3d94b22
Robin <robincarrupt@gmail.com>
2021-12-23 11:43:09 +0100

    Ajout des fonctions dans Grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,38 @@
 
 public class Grid {
-
+	int size;
+	int blocTab[][];
+	
+	Grid(int size)
+	{
+		this.size = size;
+		blocTab = new int[size][size];
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				blocTab[j][i] = -1;
+			}
+		}
+		
+	}
+	
+	public void moveBlocs(int direction)
+	{
+		
+	}
+	
+	public void createBloc()
+	{
+		
+	}
+	
+	public String drawBloc(int posX, int posY)
+	{
+		return "";
+	}
+	
+	public String drawGrid()
+	{
+		return "";
+	}
 }
