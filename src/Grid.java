e7e5a712c693898a1ab23927b6bbcfe07170c16d
Robin <robincarrupt@gmail.com>
2022-01-17 08:25:41 +0100

    Merge

--- src/Grid.java
+++ src/Grid.java
@@ -49,7 +49,7 @@ public class Grid {
 	public void move_left()
 	{
 
-		//déplacement vers la gauche	
+		//dï¿½placement vers la gauche	
 		for(int l = 0;l < size;l++) // lignes
 		{
 			for(int c = 0; c < size; c++) // colonnes
@@ -57,23 +57,23 @@ public class Grid {
 				
 				if(blocTab[c][l] != -1) // si la case contient un bloc
 				{
-					// on va lire toutes les colonnes qui précèdent ce bloc, et si on détecte du vide ou un bloc semblable, on effectue un décalage
+					// on va lire toutes les colonnes qui prï¿½cï¿½dent ce bloc, et si on dï¿½tecte du vide ou un bloc semblable, on effectue un dï¿½calage
 					
-					int idx = c; // on mémorise la colonne à laquelle on se trouve
+					int idx = c; // on mï¿½morise la colonne ï¿½ laquelle on se trouve
 					
 					while(idx >= 0 &&  (blocTab[idx][l] == -1 || blocTab[idx][l] == blocTab[c][l]))
 					{idx --;}
 										
-					if(c != idx+1)// si on a au moins bougé d'une case
+					if(c != idx+1)// si on a au moins bougï¿½ d'une case
 					{
-						if(blocTab[idx+1][l] == blocTab[c][l]) // si le dernier bloc a la même valeur que le bloc actuel
+						if(blocTab[idx+1][l] == blocTab[c][l]) // si le dernier bloc a la mï¿½me valeur que le bloc actuel
 						{
 							blocTab[idx+1][l] *= 2; // on double la valeur de la case du fond
 							blocTab[c][l] = -1;//on vide l'ancienne case du bloc
 						}
 						else
 						{
-							blocTab[idx+1][l] = blocTab[c][l];//on déplace le bloc actuel
+							blocTab[idx+1][l] = blocTab[c][l];//on dï¿½place le bloc actuel
 							blocTab[c][l] = -1;//on vide l'ancienne case du bloc
 						}
 					}
@@ -87,7 +87,7 @@ public class Grid {
 	
 	public void move_up()
 	{
-		//déplacement vers le haut	
+		//dï¿½placement vers le haut	
 		for(int l = 0;l < size;l++) // lignes
 		{
 			for(int c = 1; c < size; c++) // colonnes 
@@ -95,23 +95,23 @@ public class Grid {
 				
 				if(blocTab[l][c] != -1) // si la case contient un bloc
 				{
-					// on va lire toutes les colonnes qui précèdent ce bloc, et si on détecte du vide ou un bloc semblable, on effectue un décalage
+					// on va lire toutes les colonnes qui prï¿½cï¿½dent ce bloc, et si on dï¿½tecte du vide ou un bloc semblable, on effectue un dï¿½calage
 					
-					int idx = c; // on mémorise la colonne à laquelle on se trouve
+					int idx = c; // on mï¿½morise la colonne ï¿½ laquelle on se trouve
 					
 					while(idx >= 0 &&  (blocTab[l][idx] == -1 || blocTab[l][idx] == blocTab[l][c]))
 					{idx --;}
 					
-					if(c != idx+1)// si on a au moins bougé d'une case
+					if(c != idx+1)// si on a au moins bougï¿½ d'une case
 					{
-						if(blocTab[l][idx+1] == blocTab[l][c]) // si le dernier bloc a la même valeur que le bloc actuel
+						if(blocTab[l][idx+1] == blocTab[l][c]) // si le dernier bloc a la mï¿½me valeur que le bloc actuel
 						{
 							blocTab[l][idx+1] *= 2; // on double la valeur de la case du fond
 							blocTab[l][c] = -1;//on vide l'ancienne case du bloc
 						}
 						else
 						{
-							blocTab[l][idx+1] = blocTab[l][c];//on déplace le bloc actuel
+							blocTab[l][idx+1] = blocTab[l][c];//on dï¿½place le bloc actuel
 							blocTab[l][c] = -1;//on vide l'ancienne case du bloc
 						}
 					}
@@ -124,7 +124,7 @@ public class Grid {
 	
 	public void move_right()
 	{
-		//déplacement vers la droite	
+		//dï¿½placement vers la droite	
 				for(int l = size-1;l >= 0;l--) // lignes
 				{
 					for(int c = size-1; c >= 0; c--) 
@@ -132,23 +132,23 @@ public class Grid {
 						
 						if(blocTab[c][l] != -1) // si la case contient un bloc
 						{
-							// on va lire toutes les colonnes qui précèdent ce bloc, et si on détecte du vide ou un bloc semblable, on effectue un décalage
+							// on va lire toutes les colonnes qui prï¿½cï¿½dent ce bloc, et si on dï¿½tecte du vide ou un bloc semblable, on effectue un dï¿½calage
 							
-							int idx = c; // on mémorise la colonne à laquelle on se trouve
+							int idx = c; // on mï¿½morise la colonne ï¿½ laquelle on se trouve
 							
 							while(idx <= size-1 &&  (blocTab[idx][l] == -1 || blocTab[idx][l] == blocTab[c][l]))
 							{idx ++;}
 							
-							if(c != idx-1)// si on a au moins bougé d'une case
+							if(c != idx-1)// si on a au moins bougï¿½ d'une case
 							{
-								if(blocTab[idx-1][l] == blocTab[c][l]) // si le dernier bloc a la même valeur que le bloc actuel
+								if(blocTab[idx-1][l] == blocTab[c][l]) // si le dernier bloc a la mï¿½me valeur que le bloc actuel
 								{
 									blocTab[idx-1][l] *= 2; // on double la valeur de la case du fond
 									blocTab[c][l] = -1;//on vide l'ancienne case du bloc
 								}
 								else
 								{
-									blocTab[idx-1][l] = blocTab[c][l];//on déplace le bloc actuel
+									blocTab[idx-1][l] = blocTab[c][l];//on dï¿½place le bloc actuel
 									blocTab[c][l] = -1;//on vide l'ancienne case du bloc
 								}
 							}
@@ -169,23 +169,23 @@ public class Grid {
 				
 				if(blocTab[l][c] != -1) // si la case contient un bloc
 				{
-					// on va lire toutes les colonnes qui précèdent ce bloc, et si on détecte du vide ou un bloc semblable, on effectue un décalage
+					// on va lire toutes les colonnes qui prï¿½cï¿½dent ce bloc, et si on dï¿½tecte du vide ou un bloc semblable, on effectue un dï¿½calage
 					
-					int idx = c; // on mémorise la colonne à laquelle on se trouve
+					int idx = c; // on mï¿½morise la colonne ï¿½ laquelle on se trouve
 					
 					while(idx <= size-1 &&  (blocTab[l][idx] == -1 || blocTab[l][idx] == blocTab[l][c]))
 					{idx ++;}
 										
-					if(c != idx-1)// si on a au moins bougé d'une case
+					if(c != idx-1)// si on a au moins bougï¿½ d'une case
 					{
-						if(blocTab[l][idx-1] == blocTab[l][c]) // si le dernier bloc a la même valeur que le bloc actuel
+						if(blocTab[l][idx-1] == blocTab[l][c]) // si le dernier bloc a la mï¿½me valeur que le bloc actuel
 						{
 							blocTab[l][idx-1] *= 2; // on double la valeur de la case du fond
 							blocTab[l][c] = -1;//on vide l'ancienne case du bloc
 						}
 						else
 						{
-							blocTab[l][idx-1] = blocTab[l][c];//on déplace le bloc actuel
+							blocTab[l][idx-1] = blocTab[l][c];//on dï¿½place le bloc actuel
 							blocTab[l][c] = -1;//on vide l'ancienne case du bloc
 						}
 					}
@@ -232,12 +232,12 @@ public class Grid {
 	
 	public boolean is2048()
 	{
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if(blocTab[j][i] == 2048) return true;
+			}
+		}
 		return false;
-	}
-	
-	public boolean replay()
-	{
-		return false;
-	}
+	}	
 	
 }
